6f8df7a900afe7889d5b12a57cd7aa8d7416ebb0
kai.wang <kai.wang.john@gmail.com>
2013-12-02 17:00:29 +0800

    添加数据库文件

---
+++ doc/psy-web.sql
@@ -0,0 +1,135 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50614
+Source Host           : localhost:3306
+Source Database       : psy-web
+
+Target Server Type    : MYSQL
+Target Server Version : 50614
+File Encoding         : 65001
+
+Date: 2013-11-28 18:49:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `order` int(3) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_IP` varchar(255) DEFAULT NULL,
+  `content` text,
+  `parent` int(11) DEFAULT '0',
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`),
+  KEY `user` (`user_id`),
+  KEY `post` (`post_id`),
+  CONSTRAINT `post` FOREIGN KEY (`post_id`) REFERENCES `post` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for meta
+-- ----------------------------
+DROP TABLE IF EXISTS `meta`;
+CREATE TABLE `meta` (
+  `key` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of meta
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for post
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` longtext,
+  `comment_count` int(11) DEFAULT '0',
+  `status` int(2) DEFAULT '1',
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of post
+-- ----------------------------
+INSERT INTO `post` VALUES ('1', '1', '2', '资源应该有特殊标记如：<source:>source id</source>', '0', '1', '2013-11-24 19:41:53', '2013-11-24 19:46:22');
+
+-- ----------------------------
+-- Table structure for post_category
+-- ----------------------------
+DROP TABLE IF EXISTS `post_category`;
+CREATE TABLE `post_category` (
+  `post_id` int(11) NOT NULL DEFAULT '0',
+  `category_id` int(11) DEFAULT NULL,
+  KEY `category_post_category` (`category_id`),
+  KEY `post_category` (`post_id`),
+  CONSTRAINT `post_category` FOREIGN KEY (`post_id`) REFERENCES `post` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `category_post_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of post_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `avatar` int(11) DEFAULT NULL,
+  `type` int(2) DEFAULT NULL,
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'John', null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('2', 'Jack', null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('3', 'Peter', null, 'peter@peter.com', null, null, null, null);
+INSERT INTO `user` VALUES ('5', 'Peter', null, 'peter@peter.com', null, null, null, null);
+INSERT INTO `user` VALUES ('6', 'Punny', null, 'peter@peter.com', null, null, '2013-11-24 18:10:46', '2013-11-24 18:11:37');

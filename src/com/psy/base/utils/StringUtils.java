48681130958df8496f05b94ac43dad9905e7c3f6
kai.wang <kai.wang.john@gmail.com>
2013-12-03 13:54:50 +0800

    添加isEmpty方法

--- src/com/psy/base/utils/StringUtils.java
+++ src/com/psy/base/utils/StringUtils.java
@@ -4,4 +4,10 @@ package com.psy.base.utils;
  * Created by kai.wang on 12/3/13.
  */
 public class StringUtils {
+	public static boolean isEmpty(String str) {
+		if (str == null || "".equals(str)) {
+			return true;
+		}
+		return false;
+	}
 }

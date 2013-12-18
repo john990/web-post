c9057de21ad66954a683d6025aff0a73be8ecd68
kai.wang <kai.wang.john@gmail.com>
2013-12-18 15:52:11 +0800

    首页

---
+++ src/com/psy/common/Utils.java
@@ -0,0 +1,15 @@
+package com.psy.common;
+
+/**
+ * Created by kai.wang on 12/18/13.
+ */
+public class Utils {
+
+	/** 转换page参数 */
+	public static int parsePage(int page){
+		if(page < 1){
+			page = 1;
+		}
+		return page;
+	}
+}
